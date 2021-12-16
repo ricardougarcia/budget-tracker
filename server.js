ffd58b95724e33a979b785f1b8936c9fee3b1695
Ricardo Garcia <ricardougarcia@gmail.com>
2021-12-16 10:09:24 -0500

    began db.js file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/ricos-database",
+  process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-db",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
